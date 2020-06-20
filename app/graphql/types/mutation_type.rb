2904e372adf41a898d64663febc3843d461b1329
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-06-20 19:31:31 -0300

    Add mutation to sign in

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
-    field :Create_user, mutation: Mutations::CreateUser, description: 'Creates a new user'
+    field :create_user, mutation: Mutations::CreateUser, description: 'Creates a new user'
+    field :sign_in_user, mutation: Mutations::SignInUser, description: 'Signs in an already created user'
     field :create_link, mutation: Mutations::CreateLink, description: 'Insert a new link into database'
   end
 end

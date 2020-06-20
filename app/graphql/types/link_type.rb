5a3b974401bb1d763468120c99c64cdd2a14c20c
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-06-20 20:06:28 -0300

    Add belongs_to :user relationship to Link

--- app/graphql/types/link_type.rb
+++ app/graphql/types/link_type.rb
@@ -3,5 +3,7 @@ module Types
     field :id, ID, null: false
     field :url, String, null: false
     field :description, String, null: false
+    # "method" option remaps field to an attribute of Link model
+    field :posted_by, UserType, null: true, method: :user
   end
 end

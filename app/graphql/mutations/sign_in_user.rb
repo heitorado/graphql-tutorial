cce988a245c69ccaa613a7210fd7d9571bc3f167
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-06-20 19:52:19 -0300

    Adds basic controller authentication and current_user method

--- app/graphql/mutations/sign_in_user.rb
+++ app/graphql/mutations/sign_in_user.rb
@@ -18,6 +18,9 @@ module Mutations
       crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
       token = crypt.encrypt_and_sign("user-id:#{user.id}")
 
+      # stores token in session
+      context[:session][:token] = token
+
       { user: user, token: token }
     end
   end

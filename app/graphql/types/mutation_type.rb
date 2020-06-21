aa787f066dbfd60c75d2cbb9b204784e151b6379
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2020-06-21 16:17:58 -0300

    Add mutation for voting on a link

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,7 +1,8 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::CreateUser, description: 'Creates a new user'
-    field :sign_in_user, mutation: Mutations::SignInUser, description: 'Signs in an already created user'
     field :create_link, mutation: Mutations::CreateLink, description: 'Insert a new link into database'
+    field :create_vote, mutation: Mutations::CreateVote, description: 'Adds a vote for a link'
+    field :sign_in_user, mutation: Mutations::SignInUser, description: 'Signs in an already created user'
   end
 end
